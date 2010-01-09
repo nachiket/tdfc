a258e3a984ba1725b2f51ed931fc01aa2fb0accd
Nachiket Kapre <nachiket@gmail.com>
2010-01-08 20:42:31 -0500

    ok?

--- src/bindvalues.cc
+++ src/bindvalues.cc
@@ -408,6 +408,7 @@ Expr *EvaluateExpr(Expr *orig)
       {
 	ExprLValue *lexpr=(ExprLValue *)orig;
 	Symbol *lsym=lexpr->getSymbol();
+	cout << "debug:" << lexpr->toString() << "," << lsym->getAnnote(ANNOTE_IS_READ) << endl;
 	if (  lsym->isParam() ||
 	    ( lsym->getAnnote(ANNOTE_IS_READ) &&
 	     !lsym->getAnnote(ANNOTE_IS_WRITTEN)))
@@ -436,6 +437,7 @@ Expr *EvaluateExpr(Expr *orig)
 	    else {
 	      // - lexpr is not subscripted array --> get symbol value
 	      value = ((SymbolVar*)lsym)->getValue();
+	      cout << "Value=" << value << endl;
 	    }
 	    if (value==(Expr*)NULL)
 	      {
@@ -444,6 +446,7 @@ Expr *EvaluateExpr(Expr *orig)
 		  << " in " << lsym->getOperator()->getName()
 		  << " has no value " << endl;
 		  */
+		  cout << "Huh?" << endl;
 		return(orig);
 	      }
 	    else
