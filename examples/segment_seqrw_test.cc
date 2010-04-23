1cf6ca3d77f250c3d2afe4d059f3b01083ef65ba
Nachiket Kapre <nachiket@gmail.com>
2010-04-22 21:10:57 -0400

    test the seq rw segment

---
+++ examples/segment_seqrw_test.cc
@@ -0,0 +1,44 @@
+#include "ScoreStream.h"
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include "segment_seqrw_compose.h"
+using namespace std;
+
+ofstream* ScoreOperator::fout=NULL;
+
+int main(int argc, char *argv[]) {
+
+    score_init();
+
+    UNSIGNED_SCORE_SEGMENT contents = NEW_UNSIGNED_SCORE_SEGMENT(16, 16);
+    for(int i=0;i<16;i++) {
+	SEGMENT_WRITE(contents, 100+i,i);
+    }
+    UNSIGNED_SCORE_STREAM rddata = NEW_UNSIGNED_SCORE_STREAM(16);
+    UNSIGNED_SCORE_STREAM wrdata = NEW_UNSIGNED_SCORE_STREAM(16);
+
+    NEW_segment_seqrw_compose(contents, rddata, wrdata);
+    
+    int index=0;
+    while(1) {
+        // wait for something to be inserted into rddata
+    	while(STREAM_EMPTY(rddata)) {
+	}
+
+	long long int data=STREAM_READ_NOACC(rddata);
+	cout << "Read data item=" << data << endl;
+
+	// wait for non-full condition
+	while(STREAM_FULL(wrdata)) {
+	}
+
+	data=200+index;
+	STREAM_WRITE_NOACC(wrdata, data);	  
+	cout << "Wrote data item=" << data << endl;
+	index++;	
+    }
+
+    score_exit();
+}
+
