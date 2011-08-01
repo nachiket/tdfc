2d0dbf7ca5681a77a81b99d6a731116e081ee7de
Nachiket Kapre <nachiket@ieee.org>
2011-08-01 16:08:53 +0100

    changed add to simple example..

--- examples/addtest.cpp
+++ examples/addtest.cpp
@@ -10,41 +10,29 @@ using namespace std;
 
 int main() {
 	score_init();
-	cout << "I am here"<<endl;
+	
+	// stream definitions
 	DOUBLE_SCORE_STREAM a=NEW_DOUBLE_SCORE_STREAM();
 	DOUBLE_SCORE_STREAM b=NEW_DOUBLE_SCORE_STREAM();
-	BOOLEAN_SCORE_STREAM c=NEW_BOOLEAN_SCORE_STREAM();
-	//DOUBLE_SCORE_STREAM d=NEW_BOOLEAN_SCORE_STREAM();
-	DOUBLE_SCORE_STREAM d=NEW_DOUBLE_SCORE_STREAM();
+	DOUBLE_SCORE_STREAM c=NEW_DOUBLE_SCORE_STREAM();
 
-	NEW_add(a,b,c,d);
+	// instantiating the ADDER..
+	NEW_add(a,b,c);
 
+	// test inputs
 	STREAM_WRITE_NOACC(a, 1.0);
 	STREAM_WRITE_NOACC(b, 1.0);
-	STREAM_WRITE(c, 1);
-	STREAM_WRITE_NOACC(a, 1.0);
-	STREAM_WRITE_NOACC(b, 1.0);
-	STREAM_WRITE(c, 1);
-	STREAM_WRITE_NOACC(a, 1.0);
-	STREAM_WRITE_NOACC(b, 1.0);
-	STREAM_WRITE(c, 0);
-	STREAM_WRITE_NOACC(a, 1.0);
-	STREAM_WRITE_NOACC(b, 1.0);
-	STREAM_WRITE(c, 0);
+	
+	STREAM_WRITE_NOACC(a, 2.0);
+	STREAM_WRITE_NOACC(b, 3.0);
 
-//	STREAM_CLOSE(a);
-//	STREAM_CLOSE(b);
 	sleep(1);
 	
-	cout << "I am here"<<endl;
 	bool done=false;
 	while(!done) {
-    	        cout << "I am here"<<endl;
-		done=STREAM_EOS(d);
-		cout<<done<<endl;		
-		cout << "d=" << STREAM_READ_NOACC(d) << endl;
+		done=STREAM_EOS(c);
+		cout << "c=" << STREAM_READ_NOACC(c) << endl;
 	}
 
-	
 	score_exit();
 }
